308662d52baeee0c809ccfb9b762fa4d02331129
youlik <tony@gmail.com>
2019-06-06 21:27:01 +0800

    路由

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http=require("http");
-http.createServer(function(request,response){
-    
-    response.writeHead(200,{'Content-Type':'text/plain'});
+var url=require("url");
+ 
+function start(route){
+    function onRequest(request,response) {
+        var pathname=url.parse(request.url).pathname;
+        console.log("Request for"+pathname+"recevied.");
+        route(pathname);
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write("Hello World");
+        response.end()
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started");
+}
 
-    response.end('Hello World\n')
-}).listen(8888)
-console.log('Server running at http://127.0.0.1:8888/');
+exports.start=start;
