4ff4044f3733060524c93ebd250ae3347826c3db
youlik <tony@gmail.com>
2019-06-07 12:17:12 +0800

    Process 属性

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
-// 引入 events 模块
-var events = require('events');
-// 创建 eventEmitter 对象
-var eventEmitter = new events.EventEmitter();
- 
-// 创建事件处理程序
-var connectHandler = function connected() {
-   console.log('连接成功。');
-  
-   // 触发 data_received 事件 
-   eventEmitter.emit('data_received');
-}
- 
-// 绑定 connection 事件处理程序
-eventEmitter.on('connection', connectHandler);
- 
-// 使用匿名函数绑定 data_received 事件
-eventEmitter.on('data_received', function(){
-   console.log('数据接收成功。');
+// 输出到终端
+process.stdout.write("Hello World!" + "\n");
+
+// 通过参数读取
+process.argv.forEach(function(val, index, array) {
+   console.log(index + ': ' + val);
 });
- 
-// 触发 connection 事件 
-eventEmitter.emit('connection');
- 
-console.log("程序执行完毕。");
+
+// 获取执行路径
+console.log(process.execPath);
+
+
+// 平台信息
+console.log(process.platform);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url=require("url");
 function start(route){
     function onRequest(request,response) {
         var pathname=url.parse(request.url).pathname;
-        console.log("Request for"+pathname+"recevied.");
+        console.log("Request for "+pathname+" recevied.");
         route(pathname);
         response.writeHead(200,{"Content-Type":"text/plain"});
         response.write("Hello World");
