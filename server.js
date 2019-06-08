3a9fd48639eab875c13ec0ac783b1fb31c990841
youlik <tony@gmail.com>
2019-06-08 10:03:30 +0800

    express

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
- 
- 
-// 创建服务器
-http.createServer( function (request, response) {  
-   // 解析请求，包括文件名
-   var pathname = url.parse(request.url).pathname;
-   
-   // 输出请求的文件名
-   console.log("Request for " + pathname + " received.");
-   
-   // 从文件系统中读取请求的文件内容
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP 状态码: 404 : NOT FOUND
-         // Content Type: text/html
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{             
-         // HTTP 状态码: 200 : OK
-         // Content Type: text/html
-         response.writeHead(200, {'Content-Type': 'text/html'});    
-         
-         // 响应文件内容
-         response.write(data.toString());        
-      }
-      //  发送响应数据
-      response.end();
-   });   
-}).listen(8080);
- 
-// 控制台会输出以下信息
-console.log('Server running at http://127.0.0.1:8080/');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/:id', function (req, res) {
+   // 首先我们读取已存在的用户
+   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       var user = data["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
